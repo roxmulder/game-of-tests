545ad0aabc4c07baea4f011119c432d4ca511dff
roxmulder <we.are.torchwood@gmail.com>
2019-04-25 13:51:24 -0300

    Subo clase Dothraki y sus tests

--- src/main/java/got/Dothraki.java
+++ src/main/java/got/Dothraki.java
@@ -1,5 +1,17 @@
 package got;
 
 public class Dothraki implements ISoldado {
+	private int tatuajes;
 
+	public Dothraki(int tatuajes) {
+		this.tatuajes = tatuajes;
+	}
+	
+	public boolean esKamikaze() {
+		return true;
+	}
+	
+	public int potenciaDeAtaque() {
+		return 5 * tatuajes;
+	}
 }
--- src/test/java/got/DothrakiTest.java
+++ src/test/java/got/DothrakiTest.java
@@ -6,6 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class DothrakiTest {
+import org.junit.jupiter.api.BeforeEach;
 
+class DothrakiTest {
+	private Dothraki khal;
+	
+	@BeforeEach
+	void init() {
+		khal = new Dothraki(5);
+	}
+	
+	@Test
+	void test_esKamikaze() {
+		assertTrue(khal.esKamikaze(), "Testing Es kamikaze");
+	}
+	
+	@Test
+	void test_potenciaDeAtaque(){
+		assertEquals(25, khal.potenciaDeAtaque());
+	}
 }
